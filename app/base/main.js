c322b57b61779a7836c1b648731b79c23cab22ad
chqiangs <chqiangs@gmail.com>
2017-08-25 16:36:29 +0800

    调整

--- app/base/main.js
+++ app/base/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+
+var appid = 'cytbiQFxM'; 
+var conf = 'prod_1719392116e7712b7685989f9d145d1d'; 
+var changurl = "http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id="+ appid + "&conf="+ conf;
+var width = window.innerWidth || document.documentElement.clientWidth; 
+if (width > 960) { 
+    changurl="http://changyan.sohu.com/upload/changyan.js";
+}
 require.config({
     paths: {
         angular: '../bower_components/angular/angular.min',
@@ -17,7 +25,7 @@ require.config({
         marked:'../bower_components/uieditor/marked',
         htmleditor:'../bower_components/uieditor/htmleditor',
         showdown        : "../bower_components/showdown/dist/showdown.min",
-        // duoshuo:'http://static.duoshuo.com/embed',
+        duoshuo:changurl,
         buttonlite:'http://static.bshare.cn/b/buttonLite',
         bshare:'http://static.bshare.cn/b/bshareC2',
     },
@@ -32,7 +40,7 @@ require.config({
         'uikit':['jquery'],
         'htmleditor':['jquery','uikit','codemirror','marked'],
         'showdown':{'exports':'showdown'},
-        // 'duoshuo':['jquery']
+        'duoshuo':['jquery']
     },
     priority: [
         'angular',
