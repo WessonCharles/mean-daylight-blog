b701ae47eb1642b1047c82f5b1d45b5138849653
chqiangs <chqiangs@gmail.com>
2017-09-08 13:33:13 +0800

    调整

---
+++ dist/manifest.js
@@ -0,0 +1,5 @@
+var webpackManifest ={
+  "js/ts-web-app.js": "js/ts-web-app-d211ab2065.js",
+  "js/ts-web-tpl.js": "js/ts-web-tpl-7c9221dd73.js",
+  "js/ts-web-vendor.js": "js/ts-web-vendor-8b774a1ba6.js"
+}
